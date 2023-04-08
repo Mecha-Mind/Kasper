202550dc7f12a7667a4af9e480da01f68e4fb3c8
Mecha <hanishetos4@gmail.com>
2023-04-08 21:32:22 +0200

    updated some methods in js & css with clear comments

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
+// Select elements
+const navMainLinks = document.querySelector("#nav_links");
 const navLinks = document.querySelectorAll("#nav_links a");
-console.log(navLinks);
 const navToggle = document.querySelector("#nav_toggle");
 const shuffles = document.querySelectorAll(".shuffle li");
-
 const landing = document.querySelector(".landing");
-
 const bullets = document.querySelectorAll(".bts li");
+const btnScrollToTop = document.querySelector("#btn_up");
+const progressScroller = document.querySelector("#scroller");
+const slider = document.querySelector(".image-slider");
+const images = document.querySelectorAll(".image-slider img");
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+const searchIcon = document.querySelector("#search_icon");
+const searchInput = document.querySelector("#search_input");
 
-function activeLinks(links) {
+// toggle on click search icon
+searchIcon.addEventListener("click", () => {
+  searchInput.classList.toggle("show");
+});
+
+// Add active class to clicked links
+const setActiveLink = (links) => {
   links.forEach((link) => {
-    link.addEventListener("click", function () {
+    link.addEventListener("click", () => {
       links.forEach((btn) => btn.classList.remove("active"));
-      this.classList.add("active");
+      link.classList.add("active");
     });
   });
-}
-
-activeLinks(navLinks);
-activeLinks(shuffles);
-activeLinks(bullets);
-
-const navMainLinks = document.querySelector("#nav_links");
-console.log(navMainLinks);
+};
 
 navToggle.addEventListener("click", (_) => {
   navMainLinks.classList.toggle("show");
@@ -33,31 +39,63 @@ navToggle.addEventListener("click", (_) => {
 });
 
 // Show/hide scroll-to-top button
-const btnUp = document.querySelector("#btn_up");
-console.log(btnUp);
-
 window.addEventListener("scroll", () => {
   if (document.documentElement.scrollTop > 1160) {
-    btnUp.style.display = "flex";
+    btnScrollToTop.classList.add("visible");
   } else {
-    btnUp.style.display = "none";
+    btnScrollToTop.classList.remove("visible");
   }
 });
 
 // Scroll to top when button is clicked
-btnUp.addEventListener("click", () => {
+btnScrollToTop.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
 // Update scroll progress bar width
-const scroller = document.querySelector("#scroller");
-console.log(scroller);
-
 window.addEventListener("scroll", () => {
-  const height =
+  const scrollHeight =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
   const scrollTop = document.documentElement.scrollTop;
-  const scrollWidth = (scrollTop / height) * 100;
-  scroller.style.width = `${scrollWidth}%`;
+  const scrollWidth = (scrollTop / scrollHeight) * 100;
+  progressScroller.style.width = `${scrollWidth}%`;
 });
+
+// Image slider functionality
+let currentImage = 0;
+
+const showImage = (index) => {
+  images[currentImage].style.display = "none";
+  bullets[currentImage].classList.remove("active");
+  currentImage = index;
+  images[currentImage].style.display = "block";
+  bullets[currentImage].classList.add("active");
+};
+
+leftBtn.addEventListener("click", () => {
+  const lastIndex = images.length - 1;
+  const newIndex = currentImage === 0 ? lastIndex : currentImage - 1;
+  showImage(newIndex);
+});
+
+rightBtn.addEventListener("click", () => {
+  const lastIndex = images.length - 1;
+  const newIndex = currentImage === lastIndex ? 0 : currentImage + 1;
+  showImage(newIndex);
+});
+
+bullets.forEach((bullet, index) => {
+  bullet.addEventListener("click", () => {
+    showImage(index);
+  });
+});
+
+// Initialize slider
+images[currentImage].style.display = "block";
+bullets[currentImage].classList.add("active");
+
+// Initialize functionality for links
+setActiveLink(navLinks);
+setActiveLink(shuffles);
+setActiveLink(bullets);
--- ma.js
+++
@@ -1,4 +0,0 @@
-let x = "55";
-let y = "0";
-console.log(x + y);
-console.log(Math.sqrt(81));
